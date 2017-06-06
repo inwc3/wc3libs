4b3d6ca9c49aac4b7b12cb6e9c57df1525ff206c
Frotty <frotty@sunayama.de>
2017-06-06 21:56:01 +0200

    align UnitDataSLK

--- src/main/java/net/moonlightflower/wc3libs/slk/app/objs/UnitDataSLK.java
+++ src/main/java/net/moonlightflower/wc3libs/slk/app/objs/UnitDataSLK.java
@@ -1,34 +1,20 @@
 package net.moonlightflower.wc3libs.slk.app.objs;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-
 import net.moonlightflower.wc3libs.dataTypes.DataList;
 import net.moonlightflower.wc3libs.dataTypes.DataType;
 import net.moonlightflower.wc3libs.dataTypes.DataTypeInfo;
-import net.moonlightflower.wc3libs.dataTypes.Serializable;
-import net.moonlightflower.wc3libs.dataTypes.app.AIPlaceType;
-import net.moonlightflower.wc3libs.dataTypes.app.Bool;
-import net.moonlightflower.wc3libs.dataTypes.app.CombatTarget;
-import net.moonlightflower.wc3libs.dataTypes.app.DeathType;
-import net.moonlightflower.wc3libs.dataTypes.app.Int;
-import net.moonlightflower.wc3libs.dataTypes.app.ItemId;
-import net.moonlightflower.wc3libs.dataTypes.app.MoveType;
-import net.moonlightflower.wc3libs.dataTypes.app.PathingTex;
-import net.moonlightflower.wc3libs.dataTypes.app.Real;
-import net.moonlightflower.wc3libs.dataTypes.app.UnitId;
-import net.moonlightflower.wc3libs.dataTypes.app.UnitRace;
-import net.moonlightflower.wc3libs.dataTypes.app.Wc3String;
+import net.moonlightflower.wc3libs.dataTypes.app.*;
 import net.moonlightflower.wc3libs.misc.ObjId;
 import net.moonlightflower.wc3libs.slk.ObjSLK;
 import net.moonlightflower.wc3libs.slk.SLK;
 import net.moonlightflower.wc3libs.slk.SLKState;
-import net.moonlightflower.wc3libs.slk.app.objs.ItemSLK.Obj;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class UnitDataSLK extends ObjSLK<UnitDataSLK, UnitId, UnitDataSLK.Obj> {
 	public final static File GAME_USE_PATH = new File("Units\\UnitData.slk");
@@ -65,43 +51,37 @@ public class UnitDataSLK extends ObjSLK<UnitDataSLK, UnitId, UnitDataSLK.Obj> {
 		}
 		
 		public final static State<UnitId> OBJ_ID = new State<>("unitID", UnitId.class);
-		
-		public final static State<Int> ART_ORIENT_INTERPOLATION = new State<>("orientInterp", Int.class);
-		public final static State<Bool> ART_FAT_LINE_OF_SIGHT = new State<>("fatLOS", Bool.class);
-		
-		public final static State<DataList<CombatTarget>> COMBAT_TARGS = new State<>("targType", new DataTypeInfo(DataList.class, CombatTarget.class));
-		
-		public final static State<Bool> DATA_CAN_FLEE = new State<>("canFlee", Bool.class);
-		public final static State<Bool> DATA_CAN_SLEEP = new State<>("canSleep", Bool.class);
-		public final static State<Int> DATA_CARGO_USED = new State<>("cargoSize", Int.class);
-		public final static State<Real> DATA_DEATH_TIME = new State<>("death", Real.class);
-		public final static State<DeathType> DATA_DEATH_TYPE = new State<>("deathType", DeathType.class);
-		public final static State<Int> DATA_FORMATION = new State<>("formation", Int.class);
-		public final static State<Int> DATA_POINT_VALUE = new State<>("points", Int.class);
+
+		public final static State<Wc3String> EDITOR_SORT = new State<>("sort", Wc3String.class);
+		public final static State<Wc3String> EDITOR_COMMENTS = new State<>("comment(s)", Wc3String.class);
+		public final static State<UnitRace> EDITOR_RACE = new State<>("race", UnitRace.class);
 		public final static State<Int> DATA_PRIO = new State<>("prio", Int.class);
-		static final public State<Bool> DATA_STRUCTURE_CAN_BUILD_ON = new State<>("canBuildOn", Bool.class);
-		static final public State<Bool> DATA_STRUCTURE_IS_BUILD_ON = new State<>("isBuildOn", Bool.class);
 		public final static State<Bool> DATA_THREAT = new State<>("threat", Bool.class);
 		public final static State<Bool> DATA_VALID = new State<>("valid", Bool.class);
-		
-		public final static State<Wc3String> EDITOR_COMMENTS = new State<>("comment(s)", Wc3String.class);
-		public final static State<Bool> EDITOR_IN_BETA = new State<>("InBeta", Bool.class);
-		public final static State<UnitRace> EDITOR_RACE = new State<>("race", UnitRace.class);
-		public final static State<Wc3String> EDITOR_SORT = new State<>("sort", Wc3String.class);
-		public final static State<Int> EDITOR_VERSION = new State<>("version", Int.class);
-		
+		public final static State<DeathType> DATA_DEATH_TYPE = new State<>("deathType", DeathType.class);
+		public final static State<Real> DATA_DEATH_TIME = new State<>("death", Real.class);
+		public final static State<Bool> DATA_CAN_SLEEP = new State<>("canSleep", Bool.class);
+		public final static State<Int> DATA_CARGO_USED = new State<>("cargoSize", Int.class);
 		public final static State<MoveType> MOVEMENT_TYPE = new State<>("movetp", MoveType.class);
 		public final static State<Int> MOVEMENT_HEIGHT = new State<>("moveHeight", Int.class);
 		public final static State<Int> MOVEMENT_HEIGHT_MIN = new State<>("moveFloor", Int.class);
-		public final static State<Real> MOVEMENT_PROP_WINDOW = new State<>("propWin", Real.class);
 		public final static State<Real> MOVEMENT_TURN_RATE = new State<>("turnRate", Real.class);
-		
+		public final static State<Real> MOVEMENT_PROP_WINDOW = new State<>("propWin", Real.class);
+		public final static State<Int> ART_ORIENT_INTERPOLATION = new State<>("orientInterp", Int.class);
+		public final static State<Int> DATA_FORMATION = new State<>("formation", Int.class);
+		public final static State<DataList<CombatTarget>> COMBAT_TARGS = new State<>("targType", new DataTypeInfo(DataList.class, CombatTarget.class));
 		public final static State<PathingTex> PATH_TEX = new State<>("pathTex", PathingTex.class);
-		static final public State<Real> PATH_STRUCTURE_AI_PLACE_RAD = new State<>("buffRadius", Real.class);
+		public final static State<Bool> ART_FAT_LINE_OF_SIGHT = new State<>("fatLOS", Bool.class);
+		public final static State<Int> DATA_POINT_VALUE = new State<>("points", Int.class);
 		static final public State<AIPlaceType> PATH_STRUCTURE_AI_PLACE_TYPE = new State<>("buffType", AIPlaceType.class);
-		static final public State<Real> PATH_STRUCTURE_REQUIRE_WATER_RAD = new State<>("requireWaterRadius", Real.class);
-		
+		static final public State<Real> PATH_STRUCTURE_AI_PLACE_RAD = new State<>("buffRadius", Real.class);
 		public final static State<Int> TEXT_HERO_NAMES_COUNT = new State<>("nameCount", Int.class);
+		public final static State<Bool> DATA_CAN_FLEE = new State<>("canFlee", Bool.class);
+		static final public State<Real> PATH_STRUCTURE_REQUIRE_WATER_RAD = new State<>("requireWaterRadius", Real.class);
+		static final public State<Bool> DATA_STRUCTURE_IS_BUILD_ON = new State<>("isBuildOn", Bool.class);
+		static final public State<Bool> DATA_STRUCTURE_CAN_BUILD_ON = new State<>("canBuildOn", Bool.class);
+		public final static State<Bool> EDITOR_IN_BETA = new State<>("InBeta", Bool.class);
+		public final static State<Int> EDITOR_VERSION = new State<>("version", Int.class);
 	}
 	
 	public static class Obj extends SLK.Obj<UnitId> {
