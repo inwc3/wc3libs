db7eebb241fde06e5e36740050dd2054ddd63098
Frotty <frotty@sunayama.de>
2017-11-14 14:45:02 +0100

    Update test

--- src/test/java/W3ITest.java
+++ src/test/java/W3ITest.java
@@ -18,20 +18,29 @@ public class W3ITest extends Wc3LibTest {
 
         temp.delete();
 
-        Assert.assertEquals(w3I.getHeight(), w3I2.getHeight());
-        Assert.assertEquals(w3I.getWidth(), w3I2.getWidth());
-        Assert.assertEquals(w3I.getMarginBottom(), w3I2.getMarginBottom());
-        Assert.assertEquals(w3I.getMarginTop(), w3I2.getMarginTop());
-        Assert.assertEquals(w3I.getMarginLeft(), w3I2.getMarginLeft());
-        Assert.assertEquals(w3I.getMarginRight(), w3I2.getMarginRight());
-        Assert.assertEquals(Arrays.toString(w3I.getPlayers().toArray()), Arrays.toString(w3I2.getPlayers().toArray()));
-        Assert.assertEquals(w3I.getPrologueScreen().toString(), w3I2.getPrologueScreen().toString());
-        Assert.assertEquals(w3I.getLoadingScreen().toString(), w3I2.getLoadingScreen().toString());
-        Assert.assertEquals(w3I.getMapDescription(), w3I2.getMapDescription());
-        Assert.assertEquals(w3I.getFlags().toString(), w3I2.getFlags().toString());
-        Assert.assertEquals(w3I.getSavesAmount(), w3I2.getSavesAmount());
-        Assert.assertEquals(w3I.getTileset().toString(), w3I2.getTileset().toString());
-        Assert.assertEquals(w3I.getGameDataSet().toString(), w3I2.getGameDataSet().toString());
-        Assert.assertEquals(w3I.getWaterColor().toString(), w3I2.getWaterColor().toString());
+        Assert.assertEquals(w3I2.getHeight(), w3I.getHeight());
+        Assert.assertEquals(w3I2.getWidth(), w3I.getWidth());
+        Assert.assertEquals(w3I2.getMarginBottom(), w3I.getMarginBottom());
+        Assert.assertEquals(w3I2.getMarginTop(), w3I.getMarginTop());
+        Assert.assertEquals(w3I2.getMarginLeft(), w3I.getMarginLeft());
+        Assert.assertEquals(w3I2.getMarginRight(), w3I.getMarginRight());
+        Assert.assertEquals(Arrays.toString(w3I2.getPlayers().toArray()), Arrays.toString(w3I.getPlayers().toArray()));
+        Assert.assertEquals(w3I2.getPrologueScreen().toString(), w3I.getPrologueScreen().toString());
+        Assert.assertEquals(w3I2.getLoadingScreen().toString(), w3I.getLoadingScreen().toString());
+        Assert.assertEquals(w3I2.getMapDescription(), w3I.getMapDescription());
+        Assert.assertEquals(w3I2.getFlags().toString(), w3I.getFlags().toString());
+        Assert.assertEquals(w3I2.getSavesAmount(), w3I.getSavesAmount());
+        Assert.assertEquals(w3I2.getTileset().toString(), w3I.getTileset().toString());
+        Assert.assertEquals(w3I2.getGameDataSet().toString(), w3I.getGameDataSet().toString());
+        Assert.assertEquals(w3I2.getWaterColor().toString(), w3I.getWaterColor().toString());
+        Assert.assertEquals(Arrays.toString(w3I2.getUpgradeMods().toArray()), Arrays.toString(w3I.getUpgradeMods().toArray()));
+        Assert.assertEquals(Arrays.toString(w3I2.getTechMods().toArray()), Arrays.toString(w3I.getTechMods().toArray()));
+        Assert.assertEquals(Arrays.toString(w3I2.getForces().toArray()), Arrays.toString(w3I.getForces().toArray()));
+        Assert.assertEquals(Arrays.toString(w3I2.getItemTables().toArray()), Arrays.toString(w3I.getItemTables().toArray()));
+        Assert.assertEquals(Arrays.toString(w3I2.getUnitTables().toArray()), Arrays.toString(w3I.getUnitTables().toArray()));
+        Assert.assertEquals(w3I2.getPlayersRecommendedAmount(), w3I.getPlayersRecommendedAmount());
+        Assert.assertEquals(w3I2.getWorldBounds().toString(), w3I.getWorldBounds().toString());
+        Assert.assertEquals(w3I2.getTerrainFog().toString(), w3I.getTerrainFog().toString());
+        Assert.assertEquals(w3I2.getGlobalWeatherId().hashCode(), w3I.getGlobalWeatherId().hashCode());
     }
 }
