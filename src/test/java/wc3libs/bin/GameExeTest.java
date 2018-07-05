81a59df2d91547ee78cf9cae17cd8988c84097b0
WaterKnight <awaterknight@googlemail.com>
2018-07-05 16:05:30 +0200

    GameExeTest: skip when wrong platform

--- src/test/java/wc3libs/bin/GameExeTest.java
+++ src/test/java/wc3libs/bin/GameExeTest.java
@@ -1,8 +1,10 @@
 package wc3libs.bin;
 
+import com.esotericsoftware.minlog.Log;
 import net.moonlightflower.wc3libs.bin.GameExe;
 import net.moonlightflower.wc3libs.misc.Registry;
 import net.moonlightflower.wc3libs.port.MpqPort;
+import net.moonlightflower.wc3libs.port.Orient;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -11,6 +13,12 @@ import java.io.IOException;
 public class GameExeTest {
     @Test()
     public void testReg() throws IOException {
+        if (!Orient.isWindowsSystem()) {
+            Log.info("not a windows system (" + Orient.getSystem() + "), skip setGet tests");
+
+            return;
+        }
+
         GameExe gameExe = GameExe.fromRegistry();
 
         if (gameExe != null) System.out.println(gameExe.getVersion());
