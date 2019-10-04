b8d7ea31e797206c1deffae0af2ddb141a203069
Frotty <frotty@sunayama.de>
2019-10-04 21:09:24 +0200

    Add editorconfig and fix #45

--- src/main/java/net/moonlightflower/wc3libs/misc/ColorCodeTransformer.java
+++ src/main/java/net/moonlightflower/wc3libs/misc/ColorCodeTransformer.java
@@ -1,17 +1,21 @@
 package net.moonlightflower.wc3libs.misc;
 
+import java.util.regex.Pattern;
+
 /**
  * Transforms wc3 colors codes |cFFFFFFFF |r to various outputs.
  */
 public class ColorCodeTransformer {
 
+    private static final Pattern COLOR_CODE_PATTERN = Pattern.compile("\\|cff(\\d{6})", Pattern.CASE_INSENSITIVE);
+    
     /**
      * Transforms all colors codes in the given input to html for display in swing
      */
     public static String transformToHtml(String input) {
         StringBuilder sb = new StringBuilder("<html>");
 
-        input = input.replaceAll("\\|cff(\\d{6})", "<font color=#$1>" );
+        input = COLOR_CODE_PATTERN.matcher(input).replaceAll("<font color=#$1>");
         input = input.replaceAll("\\|r", "</font>");
         sb.append(input);
 
