6b3695c579fc2e116311cb45e9c1146031658d99
alexandrugabrielrait <r_alexgaby@yahoo.com>
2022-07-09 16:53:55 +0300

    Allowlist removeSignature bat and sh scripts

---
+++ out/artifacts/CampaignSplitterApp/removeSignature.bat
@@ -0,0 +1,1 @@
+wsl bash removeSignature.sh
---
+++ out/artifacts/CampaignSplitterApp/removeSignature.sh
@@ -0,0 +1,2 @@
+zip -d CampaignSplitterCLI.jar 'META-INF/*.SF' 'META-INF/*.RSA' 'META-INF/*.DSA'
+zip -d CampaignSplitterGUI.jar 'META-INF/*.SF' 'META-INF/*.RSA' 'META-INF/*.DSA'
